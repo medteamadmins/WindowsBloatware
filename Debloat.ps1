f27abb81748d448d0b811779f0964cd88c782011
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2023-09-17 19:07:07 -0400

    Update Debloat.ps1
    Update the script to remove Edge from the Start Menu.

--- Debloat.ps1
+++ Debloat.ps1
@@ -30,9 +30,10 @@ Remove-Item -Path "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\Adobe of
 Remove-Item -Path "C:\ProgramData\HP\TCO" -Recurse
 Remove-Item -Path "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\TCO Certified.lnk" -Recurse
 
-#Remove MS Edge from Desktop
+#Remove MS Edge from Desktop and Start-Menu
 Remove-Item -Path "c:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk" -Recurse
 Remove-Item -Path "c:\Users\Public\Desktop\Microsoft Edge.lnk" -Recurse
+Remove-Item -Path "C:\Users\$($ENV:USERNAME)\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk" -Recurse
 
 #Remove Chat feature
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /f /v TaskbarMn /t REG_DWORD /d 0
